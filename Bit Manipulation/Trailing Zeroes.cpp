8f7fd7eaa443a1026f67d47c98ebf59d6512547a
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-08-28 19:57:40 +0530

    Update Trailing Zeroes.cpp

--- Bit Manipulation/Trailing Zeroes.cpp
+++ Bit Manipulation/Trailing Zeroes.cpp
@@ -53,9 +53,9 @@ int Solution::solve(int A) {
     
     int count=0;
     
-       while(A != 0){
+       while((A&1)== 0){
+           A = A>>1;
            count++;
-             A>>1;
        }
     
     return count;
