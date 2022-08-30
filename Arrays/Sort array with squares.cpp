a6e3b54119b1978965cb5e3054e13f80543e080e
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-08-30 18:31:04 +0530

    Update Sort array with squares.cpp

--- Arrays/Sort array with squares.cpp
+++ Arrays/Sort array with squares.cpp
@@ -1,36 +1,36 @@
-Problem Description
+// Problem Description
 
-Given a sorted array A containing N integers both positive and negative.
+// Given a sorted array A containing N integers both positive and negative.
 
-You need to create another array containing the squares of all the elements in A and return it in non-decreasing order.
+// You need to create another array containing the squares of all the elements in A and return it in non-decreasing order.
 
-Try to this in O(N) time.
-
-
-Problem Constraints
-1 <= N <= 105.
-
--103 <= A[i] <= 103
+// Try to this in O(N) time.
 
 
+// Problem Constraints
+// 1 <= N <= 105.
 
-Input Format
-First and only argument is an integer array A.
+// -103 <= A[i] <= 103
 
 
 
-Output Format
-Return a integer array as described in the problem above.
+// Input Format
+// First and only argument is an integer array A.
 
 
 
-Example Input
-Input 1:
+// Output Format
+// Return a integer array as described in the problem above.
 
- A = [-6, -3, -1, 2, 4, 5]
-Input 2:
 
- A = [-5, -4, -2, 0, 1]
+
+// Example Input
+// Input 1:
+
+//  A = [-6, -3, -1, 2, 4, 5]
+// Input 2:
+
+//  A = [-5, -4, -2, 0, 1]
 
 
 Example Output
