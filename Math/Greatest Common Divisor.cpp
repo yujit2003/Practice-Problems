e385b5d6805e42a40dad1311bb4441469a67c76f
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-04 17:34:28 +0530

    Update Greatest Common Divisor.cpp

--- Math/Greatest Common Divisor.cpp
+++ Math/Greatest Common Divisor.cpp
@@ -1,13 +1,19 @@
-Given 2 non negative integers m and n, find gcd(m, n)
+// Given 2 non negative integers m and n, find gcd(m, n)
 
-GCD of 2 integers m and n is defined as the greatest integer g such that g is a divisor of both m and n.
+// GCD of 2 integers m and n is defined as the greatest integer g such that g is a divisor of both m and n.
 
-Both m and n fit in a 32 bit signed integer.
+// Both m and n fit in a 32 bit signed integer.
 
-Example
+// Example
 
-m : 6
-n : 9
+// m : 6
+// n : 9
 
-GCD(m, n) : 3 
-NOTE : DO NOT USE LIBRARY FUNCTIONS
+// GCD(m, n) : 3 
+// NOTE : DO NOT USE LIBRARY FUNCTIONS
+
+int Solution::gcd(int A, int B) {
+    if(B==0) return A;
+    else
+    return gcd(B,A%B);
+}
