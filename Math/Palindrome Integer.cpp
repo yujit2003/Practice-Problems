fc6829d7f6262e4e95558be9839c059a07202b6d
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-10 15:59:08 +0530

    Update Palindrome Integer.cpp

--- Math/Palindrome Integer.cpp
+++ Math/Palindrome Integer.cpp
@@ -14,3 +14,23 @@
 
 // Input : 123
 // Output : 0
+
+
+
+int Solution::isPalindrome(int A) {
+    
+    int rev,sum=0;
+    int temp = A;
+    
+    while(A>0){
+       rev=A%10;
+        A=A/10;
+       sum=sum*10+rev;
+     
+       
+    }
+    
+    return temp==sum;
+    
+     
+}
