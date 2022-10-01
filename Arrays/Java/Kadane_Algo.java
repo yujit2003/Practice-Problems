ca1eeef4c663859cfb647d8e802b852280628e31
Spardha-hash <spardha5102000@gmal.com>
2022-10-02 00:26:39 +0530

    Kadane's Algo

--- Arrays/Java/Kadane_Algo.java
+++ Arrays/Java/Kadane_Algo.java
@@ -1,19 +1,22 @@
+//Kadane algo to return the largest the maximum sum of contiguous sub array
+//For Ex -> arr[] = [-5, 4, 6, -3, 4, -1] Output -> 11
+
 import java.util.Scanner;
 
 public class Kadane_Algo {
-    public static void main(String[] args){
+    public static void main(String[] args){				// main function
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt();						//taking inputs nno. of elements and array
         int arr[]= new int[n];
         for(int i = 0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        Algo obj = new Algo();
+        Algo obj = new Algo();						 //creating object
         System.out.println(obj.maxSumArray(arr, n));
     }
 }
 class Algo{
-    int maxSumArray(int a[], int n){
+    int maxSumArray(int a[], int n){					//function that have logic of the algorithm
         int maxSum = 0;
         int currSum = 0;
         for(int i = 0; i<n;i++){
